f189b55446d500d4328d3988d70a2275557b7717
tungnk123 <doanthanhtungnk123@gmail.com>
2024-05-05 11:08:11 +0700

    Add tab for music screen

--- app/src/main/java/com/example/melodydiary/ui/music/MusicScreen.kt
+++ app/src/main/java/com/example/melodydiary/ui/music/MusicScreen.kt
@@ -1,6 +1,8 @@
 package com.example.melodydiary.ui.music
 
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,7 +14,9 @@ import androidx.compose.material.TabRow
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,19 +31,49 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.melodydiary.MelodyDiaryApp
 import com.example.melodydiary.R
+import com.example.melodydiary.model.Diary
+import com.example.melodydiary.ui.diary.Calendar
 import com.example.melodydiary.ui.diary.DiaryItem
+import com.example.melodydiary.ui.diary.DiaryList
 import com.example.melodydiary.ui.diary.DiaryScreen
+import com.example.melodydiary.ui.diary.DiaryTab
+import com.example.melodydiary.ui.diary.DiaryViewModel
 import com.example.melodydiary.ui.theme.MelodyDiaryTheme
 import java.util.Calendar
 
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MusicScreen(
     modifier: Modifier = Modifier
 ) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.title_music),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                },
+
+                )
+        }
+    ) { innerPadding ->
+
+        DiaryTab(modifier = Modifier.padding(innerPadding))
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun DiaryTab(
+    modifier: Modifier = Modifier,
+) {
     var selectedTabIndex by remember { mutableStateOf(0) }
+
     Column(
         modifier = modifier
-            .background(MaterialTheme.colorScheme.background)
             .fillMaxSize()
     ) {
         TabRow(
@@ -67,23 +101,26 @@ fun MusicScreen(
                     style = MaterialTheme.typography.labelLarge
                 )
             }
-            // Add more tabs as needed
         }
 
         // Content for each tab
         when (selectedTabIndex) {
             0 -> {
-                TabContent(text = "Content for Tab 1")
-            }
 
-            1 -> {
-                TabContent(text = "Content for Tab 2")
             }
-            // Add more content for additional tabs
+            1 -> {
+
+            }
         }
     }
 }
 
+@Composable
+fun GenMusicTab(
+    modifier: Modifier = Modifier
+) {
+
+}
 
 
 @Composable
